c5dea4d8ceccf4f4dfc0c7da62bbda607ffd6ff0
henrilja <loghenrik@gmail.com>
2025-06-30 22:41:55 +0200

    1dcnn implement attempt

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import numpy as np
 import wfdb
+import torch
 
 def load_raw_data(df, sampling_rate = 100, path = '../input/ptb-xl-dataset/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1/'):
     if sampling_rate == 100:
@@ -9,3 +10,6 @@ def load_raw_data(df, sampling_rate = 100, path = '../input/ptb-xl-dataset/ptb-x
         data = [wfdb.rdsamp(path+f) for f in tqdm(df.filename_hr)]
     data = np.array([signal for signal, _ in data])
     return data
+
+def to_tensor(x):
+    return torch.tensor(x if isinstance(x, np.ndarray) else x.values, dtype=torch.float32)
